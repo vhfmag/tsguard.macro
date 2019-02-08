3ab59cc0441d71947ff2feabccc445dee8539add
Victor Magalhães <victorhfmag@gmail.com>
2019-02-08 18:12:12 -0300

    Make generated type guards composable, fix bugs

--- src/macro.js
+++ src/macro.js
@@ -1,72 +1,69 @@
 const { createMacro, MacroError } = require("babel-plugin-macros");
-const babylon = require("babylon");
+const parser = require("@babel/parser");
 const t = require("@babel/types");
 const { addNamespace } = require("@babel/helper-module-imports");
 
 module.exports = createMacro(typeGuardMacro);
 
-// lacking: TSTupleType | TSOptionalType | TSRestType | TSConditionalType | TSInferType | TSParenthesizedType | TSTypeOperator | TSIndexedAccessType | TSMappedType | TSExpressionWithTypeArguments
+// lacking: TSTupleType | TSOptionalType | TSRestType | TSConditionalType | TSInferType | TSTypeOperator | TSIndexedAccessType | TSMappedType | TSExpressionWithTypeArguments
 // not sure on how to handle those: TSVoidKeyword | TSThisType | TSConstructorType | TSTypeReference | TSTypePredicate | TSTypeQuery |
 
 /**
  *
  *
  * @param {import("@babel/types").TSType} type
- * @param {string} identifier
  * @param {string} tg
  * @returns {string}
  */
-function typeToGuard(type, identifier, tg) {
+function typeToPartialGuard(type, tg) {
   if (t.isTSNeverKeyword(type)) {
-    return "false";
+    return "(() => false)";
   } else if (t.isTSAnyKeyword(type) || t.isTSUnknownKeyword(type)) {
-    return "true";
+    return "(() => true)";
   } else if (t.isTSUndefinedKeyword(type)) {
-    return `${identifier} === undefined`;
+    return `${tg}.isUndefined`;
   } else if (t.isTSNullKeyword(type)) {
-    return `${identifier} === null`;
+    return `${tg}.isNull`;
   } else if (t.isTSFunctionType(type)) {
-    return `${identifier} instanceof Function`;
+    return `(v => v instanceof Function)`;
   } else if (t.isTSSymbolKeyword(type)) {
-    return `type ${identifier} === "symbol"`;
+    return `(v => type v === "symbol")`;
   } else if (t.isTSBooleanKeyword(type)) {
-    return `${tg}.isBoolean(${identifier})`;
+    return `${tg}.isBoolean`;
   } else if (t.isTSNumberKeyword(type)) {
-    return `${tg}.isNumber(${identifier})`;
+    return `${tg}.isNumber`;
   } else if (t.isTSNumberKeyword(type)) {
-    return `${tg}.isNumber(${identifier})`;
+    return `${tg}.isNumber`;
   } else if (t.isTSObjectKeyword(type)) {
-    return `${tg}.isObjectLike(${identifier})`;
+    return `${tg}.isObjectLike`;
   } else if (t.isTSStringKeyword(type)) {
-    return `${tg}.isString(${identifier})`;
+    return `${tg}.isString`;
   } else if (t.isTSLiteralType(type)) {
-    return `${identifier} === ${JSON.stringify(type.literal.value)}`;
+    return `(v => v === ${JSON.stringify(type.literal.value)})`;
   } else if (t.isTSParenthesizedType(type)) {
-    return typeToGuard(type.typeAnnotation, identifier, tg);
+    return typeToPartialGuard(type.typeAnnotation, tg);
   } else if (t.isTSArrayType(type)) {
-    return `Array.isArray(${identifier}) && ${identifier}.every(x => ${typeToGuard(
+    return `(v => Array.isArray(v) && v.every(x => ${typeToGuard(
       type.elementType,
       "x",
       tg,
-    )})`;
+    )}))`;
   } else if (t.isTSTypeLiteral(type)) {
     const propertyGuards = type.members.map(
       propType =>
-        `.withProperty("${propType.key.name}", x => ${
+        `.withProperty("${propType.key.name}", ${
           propType.optional ? "x === undefined ||" : ""
-        } ${typeToGuard(propType.typeAnnotation.typeAnnotation, "x", tg)})`,
+        } ${typeToPartialGuard(propType.typeAnnotation.typeAnnotation, tg)})`,
     );
-    return `new ${tg}.IsInterface()${propertyGuards.join(
-      "",
-    )}.get()(${identifier})`;
+    return `(new ${tg}.IsInterface()${propertyGuards.join("")}.get())`;
   } else if (t.isTSIntersectionType(type)) {
-    return type.types
-      .map(nestedType => typeToGuard(nestedType, identifier, tg))
-      .join(" & ");
+    return `${tg}.isIntersection(${type.types
+      .map(nestedType => typeToPartialGuard(nestedType, tg))
+      .join(", ")})`;
   } else if (t.isTSUnionType(type)) {
-    return type.types
-      .map(nestedType => typeToGuard(nestedType, identifier, tg))
-      .join(" | ");
+    return `${tg}.isUnion(${type.types
+      .map(nestedType => typeToPartialGuard(nestedType, tg))
+      .join(", ")})`;
   }
 
   throw new Error("Unimplemented");
@@ -88,40 +85,39 @@ function typeToGuard(type, identifier, tg) {
  * @param {MacroArgument} arg
  */
 function typeGuardMacro({ references, state, babel: { types }, source }) {
-  const path = references.default[0];
-  const callExpression = path.parent;
+  for (const path of references.default) {
+    const callExpression = path.parent;
 
-  if (
-    !t.isCallExpression(callExpression) ||
-    callExpression.typeParameters.params.length !== 1
-  ) {
-    throw new MacroError(
-      "Macro should be called as a function and passed exactly one type parameter",
-    );
+    if (
+      !t.isCallExpression(callExpression) ||
+      callExpression.typeParameters.params.length !== 1
+    ) {
+      throw new MacroError(
+        "Macro should be called as a function and passed exactly one type parameter",
+      );
+    }
+
+    const macroArgument = callExpression.arguments[0];
+
+    if (!t.isIdentifier(macroArgument)) {
+      throw new MacroError(
+        "For now, this macro works with identifiers only, sorry",
+      );
+    }
+
+    const generatedImport = addNamespace(path, "generic-type-guard");
+
+    if (!t.isIdentifier(generatedImport)) {
+      throw new MacroError("Something wrong happened at our side, sorry");
+    }
+
+    const expectedType = callExpression.typeParameters.params[0];
+    const identifier = macroArgument.name;
+    const generatedCode = `(${typeToPartialGuard(
+      expectedType,
+      generatedImport.name,
+    )})(${identifier})`;
+    const generatedAst = parser.parse(generatedCode);
+    path.parentPath.replaceWith(generatedAst.program.body[0]);
   }
-
-  const macroArgument = callExpression.arguments[0];
-
-  if (!t.isIdentifier(macroArgument)) {
-    throw new MacroError(
-      "For now, this macro works with identifiers only, sorry",
-    );
-  }
-
-  const generatedImport = addNamespace(path, "generic-type-guard");
-
-  if (!t.isIdentifier(generatedImport)) {
-    throw new MacroError("Something wrong happened at our side, sorry");
-  }
-
-  const expectedType = callExpression.typeParameters.params[0];
-  console.dir(expectedType, { depth: 5 });
-  const identifier = macroArgument.name;
-  const generatedCode = typeToGuard(
-    expectedType,
-    identifier,
-    generatedImport.name,
-  );
-  const generatedAst = babylon.parse(generatedCode);
-  path.parentPath.replaceWith(generatedAst.program.body[0]);
 }
